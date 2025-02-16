6bb5273ea61f04accef7ef1b966569719e0b5a0d
andrewgiard <andrewgiard@gmail.com>
2025-02-16 14:51:45 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,10 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('savemore-cache-v1').then(cache => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
+                'index.html',
+                'manifest.json',
+                'icons/icon-192x192.png',
+                'icons/icon-512x512.png'
             ]);
         }).then(() => self.skipWaiting()) // Forces the service worker to take control
     );
