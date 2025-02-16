2af8c20984546531aaf8377c6888c98dfe455bb0
andrewgiard <andrewgiard@gmail.com>
2025-02-16 15:17:01 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,7 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('savemore-cache-v1').then(cache => {
-            return cache.addAll([
-                'index.html',
-                'manifest.json',
-                'icons/icon-192x192.png',
-                'icons/icon-512x512.png'
-            ]);
-        }).then(() => self.skipWaiting()) // Forces the service worker to take control
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== 'savemore-cache-v1')
-                    .map(key => caches.delete(key))
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+<script>
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('service-worker.js')
+            .then(registration => console.log('Service Worker registered:', registration))
+            .catch(error => console.log('Service Worker registration failed:', error));
+    }
+</script>
